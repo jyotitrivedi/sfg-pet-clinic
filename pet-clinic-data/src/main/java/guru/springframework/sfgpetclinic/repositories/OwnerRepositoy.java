205a550cf2b182733a422408c51dd7be22b398c5
jyotitrivedi <jyotitrivedi99@gmail.com>
2020-08-21 17:04:05 +0530

    Added Spring Data JPA Repositories. closes #39

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/OwnerRepositoy.java
@@ -0,0 +1,7 @@
+package guru.springframework.sfgpetclinic.repositories;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import org.springframework.data.repository.CrudRepository;
+
+public interface OwnerRepositoy extends CrudRepository<Owner,Long> {
+}
